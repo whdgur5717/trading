d064f3fb47668d0c22050981126b7e4d871240a0
whdgur5717 <whdgur5717@gmail.com>
2026-06-04 00:50:27 +0900

    refactor(back): keep stocks module to master data

--- back/src/stocks/stocks.module.ts
+++ back/src/stocks/stocks.module.ts
@@ -1,12 +1,10 @@
 import { Module } from "@nestjs/common"
-import { KisModule } from "../kis/kis.module"
 import { SuggestionService } from "./suggestion/suggestion.service"
 import { STOCK_MASTER_DATA, stockMasterData } from "./stocks.data"
 import { StocksController } from "./stocks.controller"
 import { StocksService } from "./stocks.service"
 
 @Module({
-  imports: [KisModule],
   controllers: [StocksController],
   providers: [
     {
